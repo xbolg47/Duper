a87bc0db28c572f370f6fc7f21ed4049de697eaa
Randy Nyambi <xbolg.47@gmail.com>
2019-02-16 15:35:10 +0100

    all wired up..

--- lib/duper/application.ex
+++ lib/duper/application.ex
@@ -8,13 +8,16 @@ defmodule Duper.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Duper.Worker.start_link(arg)
       # {Duper.Worker, arg}
+        Duper.Results,
+      { Duper.PathFinder, "/home/xbolg47/Downloads" },
+        Duper.WorkerSupervisor,
+      { Duper.Gatherer, 50},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
